60143a32d8f7f291b9c21e4f7961723456013154
qi.li <787139384@qq.com>
2018-06-10 22:34:16 +0800

    位置修改

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import RotationMap from './rotationMap';
+import RotationMap from './component/rotationMap';
 
 function App() {
     return (
