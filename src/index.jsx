029f437c3ba8399b693a2f85efc993156de27323
qi.li <787139384@qq.com>
2018-06-13 23:28:56 +0800

    用 React.Children.map 不需要是数组

--- src/index.jsx
+++ src/index.jsx
@@ -6,22 +6,18 @@ import RotationMap from './component/rotationMap';
 function App() {
     return (
         <RotationMap>
-            {
-                [
-                    <div key="React" className="Rmap React">
-                        <h1>你好! React!</h1>
-                    </div>,
-                    <div key="Vue" className="Rmap Vue">
-                        <h1>你好! Vue!</h1>
-                    </div>,
-                    <div key="Ng" className="Rmap Ng">
-                        <h1>你好! NG!</h1>
-                    </div>,
-                    <div key="Component" className="Rmap Component">
-                        <h1>你好! 组件!</h1>
-                    </div>
-                ]
-            }
+            <div key="React" className="Rmap React">
+                <h1>你好! React!</h1>
+            </div>
+            <div key="Vue" className="Rmap Vue">
+                <h1>你好! Vue!</h1>
+            </div>
+            <div key="Ng" className="Rmap Ng">
+                <h1>你好! NG!</h1>
+            </div>
+            <div key="Component" className="Rmap Component">
+                <h1>你好! 组件!</h1>
+            </div>
         </RotationMap>
     );
 }
