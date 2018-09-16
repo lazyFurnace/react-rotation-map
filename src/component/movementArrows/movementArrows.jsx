a80a9e947e4297795089458c623803df62f45a39
qi.li <787139384@qq.com>
2018-09-16 23:51:58 +0800

    feat: 将原来 SVG 图标更换为字体图标，样式及效果修改

--- src/component/movementArrows/movementArrows.jsx
+++ src/component/movementArrows/movementArrows.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import '../../iconfont/iconfont.css';
 import './movementArrows.less';
 
-function MovementArrows(props) {
-    return (
-        <div className="MovementArrows">
-            <button
-                className="arrow arrow-left"
-                onClick={() => props.arrowChangeState('down')}
-            />
-            <button
-                className="arrow arrow-right"
-                onClick={() => props.arrowChangeState('up')}
-            />
-        </div>
-    );
+class MovementArrows extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            iconLeft: props.iconLeft,
+            iconRight: props.iconRight
+        };
+    }
+    render() {
+        const { iconLeft, iconRight } = this.state;
+        return (
+            <div className="movement-arrows">
+                <button
+                    className={`iconfont icon-arrow-left ${iconLeft ? 'active' : ''}`}
+                    onClick={() => this.props.arrowChangeState('down')}
+                />
+                <button
+                    className={`iconfont icon-arrow-right ${iconRight ? 'active' : ''}`}
+                    onClick={() => this.props.arrowChangeState('up')}
+                />
+            </div>
+        );
+    }
 }
 
 MovementArrows.propTypes = {
-    arrowChangeState: PropTypes.func.isRequired
+    arrowChangeState: PropTypes.func.isRequired,
+    iconLeft: PropTypes.bool,
+    iconRight: PropTypes.bool
+};
+
+MovementArrows.defaultProps = {
+    iconLeft: false,
+    iconRight: true
 };
 
 export default MovementArrows;
