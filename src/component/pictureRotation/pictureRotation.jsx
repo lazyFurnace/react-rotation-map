af2a695e6c34958e19a87db50dfa15d38dafeefc
qi.li <787139384@qq.com>
2018-09-19 22:28:49 +0800

    feat: 轮播动画 class 切换，使用 animation 关键帧动画

--- src/component/pictureRotation/pictureRotation.jsx
+++ src/component/pictureRotation/pictureRotation.jsx
@@ -1,3 +1,9 @@
+/**
+ * 轮播内容区域
+ * @param {Node} children 需要轮播的内容
+ * @param {Number} index 当前选中的页码
+ * @param {String} direction 需要切换轮播的方向
+ */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Transition from 'react-transition-group/Transition';
@@ -6,17 +12,25 @@ import './pictureRotation.less';
 
 function PictureRotation(props) {
     const { children, index, direction } = props;
-    console.log(direction);
     return (
-        <div className="pictureRotation">
+        <div className="picture-rotation">
             {
                 React.Children.map(children, (item, key) => (
                     <Transition in={index === key} timeout={300}>
                         {
                             (state) => {
-                                console.log(`${key}: ${state}`);
+                                let className;
+                                if (state === 'entered') {
+                                    className = 'rotation-center';
+                                } else if (state === 'exited') {
+                                    className = 'rotation-exit';
+                                } else if (state === 'exiting') {
+                                    className = direction === 'left' ? 'rotation-center-to-left' : 'rotation-center-to-right';
+                                } else if (state === 'entering') {
+                                    className = direction === 'left' ? 'rotation-right-to-center' : 'rotation-left-to-center';
+                                }
                                 return (
-                                    <div className={`rotation_item ${index === key ? 'rotation_center' : ''}`}>
+                                    <div className={`rotation-item ${className}`}>
                                         {item}
                                     </div>
                                 );
