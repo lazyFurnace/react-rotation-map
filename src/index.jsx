2f3a542a2432a0adff729b03a304fbf62f489dad
qi.li <787139384@qq.com>
2018-09-25 23:48:03 +0800

    feat: 新的 demo 轮播图

--- src/index.jsx
+++ src/index.jsx
@@ -9,33 +9,38 @@ if (module.hot) {
     module.hot.accept();
 }
 
-function beforeChange(from, to) {
-    console.log(`beforeChange: ${from} ${to}`);
-}
-
-function afterChange(current) {
-    console.log(`afterChange: ${current}`);
-}
-
-function App() {
-    return (
-        <div className="root">
-            <RotationMap beforeChange={beforeChange} afterChange={afterChange}>
-                <div className="Rmap React">
-                    <h1>你好! React!</h1>
-                </div>
-                <div className="Rmap Vue">
-                    <h1>你好! Vue!</h1>
-                </div>
-                <div className="Rmap Ng">
-                    <h1>你好! NG!</h1>
-                </div>
-                <div className="Rmap Component">
-                    <h1>你好! 组件!</h1>
-                </div>
-            </RotationMap>
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            carouselData: [
+                'Chaos-Warrior',
+                'Cursed-Legion',
+                'Gray-Knight',
+                'Space-Wolf',
+                'Star-Warrior',
+                'Ultimate-Warrior'
+            ]
+        };
+    }
+    beforeChange = (from, to) => {
+        console.log(`beforeChange: ${from} ${to}`);
+    }
+    afterChange = (current) => {
+        console.log(`afterChange: ${current}`);
+    }
+    render() {
+        const { carouselData } = this.state;
+        return (
+            <div className="root">
+                <RotationMap beforeChange={this.beforeChange} afterChange={this.afterChange}>
+                    {
+                        carouselData.map(item => <div key={item} alt={item} className={`react-rotation ${item}`} />)
+                    }
+                </RotationMap>
+            </div>
+        );
+    }
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
