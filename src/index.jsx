e3e850d68636b78487d2ef86a45abb7ddc0b42e9
qi.li <787139384@qq.com>
2019-01-17 01:32:29 +0800

    feat: 重写 demo

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import classNames from 'classnames';
 import Rotation from '$components/rotation';
 
 import './index.less';
@@ -13,43 +14,52 @@ class App extends React.Component {
         super(props);
         this.rotation = React.createRef();
         this.state = {
-            carouselData: [
-                'Angular',
-                'React',
-                'Vue',
-                'Webpack',
-                'Nodejs'
-            ]
+            carousel: ['Angular', 'React', 'Vue', 'Webpack', 'Nodejs'],
+            changeText: 'start'
         };
     }
     beforeChange = (from, to) => {
-        document.getElementById('console').innerText = `beforeChange: ${from} to ${to}`;
+        this.setState({ changeText: `beforeChange: ${from} to ${to}` });
     }
     afterChange = (current) => {
-        document.getElementById('console').innerText = `afterChange: ${current}`;
+        this.setState({ changeText: `afterChange: ${current}` });
+    }
+    handleClick = direction => () => {
+        const { rotation } = this;
+        rotation.current.goMove(direction);
     }
     render() {
-        const { carouselData } = this.state;
+        const { carousel, changeText } = this.state;
         return (
-            <div className="root">
-                <Rotation
-                    ref={this.rotation}
-                    autoplay
-                    timeout={3000}
-                    dots
-                    easing="linear"
-                    effect="scroll"
-                    beforeChange={this.beforeChange}
-                    afterChange={this.afterChange}
-                >
-                    {
-                        carouselData.map(item => (
-                            <div key={item} alt={item} className={`react-rotation ${item}`}>
-                                <h1>{item}</h1>
-                            </div>
-                        ))
-                    }
-                </Rotation>
+            <div>
+                <header>
+                    <h2>完整实例</h2>
+                </header>
+                <div className="complete-example">
+                    <div className="rotation-content">
+                        <Rotation
+                            ref={this.rotation}
+                            autoplay
+                            timeout={3000}
+                            dots
+                            easing="linear"
+                            effect="scroll"
+                            beforeChange={this.beforeChange}
+                            afterChange={this.afterChange}
+                        >
+                            {carousel.map(item => (
+                                <div key={item} alt={item} className={classNames('react-rotation', item)}>
+                                    <h1>{item}</h1>
+                                </div>
+                            ))}
+                        </Rotation>
+                    </div>
+                    <p className="rotation-text">{changeText}</p>
+                    <div className="rotation-button">
+                        <button onClick={this.handleClick('left')}>向左</button>
+                        <button onClick={this.handleClick('right')}>向右</button>
+                    </div>
+                </div>
             </div>
         );
     }
